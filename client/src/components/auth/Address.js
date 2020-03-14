26c62257ef1ede7489129f82dea721caf9f32717
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2020-03-14 12:47:15 +0100

    Address not functionnal API-key not well configured

--- client/src/components/auth/Address.js
+++ client/src/components/auth/Address.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Script from "react-load-script";
 
 class Address extends React.Component {
   state = {
@@ -11,11 +12,46 @@ class Address extends React.Component {
     this.setState({ [name]: value });
   };
 
+  handleScriptLoad = () => {
+    // Declare Options For Autocomplete
+    const options = { types: ["geocode", "address"] };
+
+    // Initialize Google Autocomplete
+    /*global google*/
+    this.autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById("autocomplete")
+    );
+    // Avoid paying for data that you don't need by restricting the
+    // set of place fields that are returned to just the address
+    // components and formatted address
+    this.autocomplete.setFields(["address_components", "formatted_address"]); // Fire Event when a suggested name is selected
+    this.autocomplete.addListener("place_changed", this.handlePlaceSelect);
+  };
+
+  handlePlaceSelect = () => {
+    // Extract City From Address Object
+    const addressObject = this.autocomplete.getPlace();
+    const address = addressObject.address_components;
+
+    // Check if address is valid
+    if (address) {
+      // Set State
+      this.setState({
+        address: addressObject.formatted_address
+      });
+    }
+  };
+
   render() {
     return (
       <div>
+        <Script
+          url={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`}
+          onLoad={this.handleScriptLoad}
+        />
         <label>Adresse </label>
         <input
+          id="autocomplete"
           type="text"
           name="address"
           value={this.state.address}
@@ -24,7 +60,7 @@ class Address extends React.Component {
         />
         <datalist id="list">
           {/* {this.state.results.map(address => {
-            return <option>{address}</option>;
+            return <option key={address}>{address}</option>;
           })} */}
           <option value="7 rue civiale" />
           <option value="7 rue Mitoni" />
