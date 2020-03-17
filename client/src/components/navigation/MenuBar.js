9d39aea9d0cc4404c6f73480733f2c258236218a
Gregoire Lamy <g.aderlamy@gmail.com>
2020-03-17 21:17:44 +0100

    2nd try commit nav bar

--- client/src/components/navigation/MenuBar.js
+++ client/src/components/navigation/MenuBar.js
@@ -2,98 +2,84 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Paper from '@material-ui/core/Paper';
-import Fab from '@material-ui/core/Fab';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Avatar from '@material-ui/core/Avatar';
-import MenuIcon from '@material-ui/icons/Menu';
-import AddIcon from '@material-ui/icons/Add';
-import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Fab from "@material-ui/core/Fab";
+import MenuIcon from "@material-ui/icons/Menu";
+import AddIcon from "@material-ui/icons/Add";
+import SearchIcon from "@material-ui/icons/Search";
+import MoreIcon from "@material-ui/icons/MoreVert";
 
+import { withStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  text: {
-    padding: theme.spacing(2, 2, 0),
-  },
-  paper: {
-    paddingBottom: 50,
-  },
-  list: {
-    marginBottom: theme.spacing(2),
-  },
-  subheader: {
-    backgroundColor: theme.palette.background.paper,
-  },
   appBar: {
-    top: 'auto',
-    bottom: 0,
-  },
-  grow: {
-    flexGrow: 1,
+    top: "auto",
+    bottom: 0
   },
   fabButton: {
-    position: 'absolute',
+    position: "absolute",
     zIndex: 1,
     top: -30,
     left: 0,
     right: 0,
-    margin: '0 auto',
+    margin: "0 auto",
+    backgroundColor: "#F7DC00"
   },
+  root: {
+    backgroundColor: "white",
+    color: "#FFFFFF",
+    height: "70px"
+  }
+  // buttons: {
+  //   margin: "0 auto",
+  // }
 }));
 
-
-export default function MenuBar() {
+function MenuBar() {
   const classes = useStyles();
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar position="fixed" color="primary" className={classes.appBar}>
+      <AppBar
+        position="fixed"
+        color="primary"
+        className={classes.appBar}
+        classes={{ root: classes.root }}
+      >
         <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="open drawer">
             <MenuIcon />
           </IconButton>
-          <Fab color="secondary" aria-label="add" className={classes.fabButton}>
-            <AddIcon />
-          </Fab>
-          <div className={classes.grow} />
-          <IconButton color="inherit">
-            <SearchIcon />
-          </IconButton>
-          <IconButton edge="end" color="inherit">
-            <MoreIcon />
-          </IconButton>
+          <Link to="/new-donation">
+            <Fab
+              color="secondary"
+              aria-label="add"
+              className={classes.fabButton}
+            >
+              <AddIcon />
+            </Fab>
+          </Link>
+          <Link to="/dashboard" className="buttons">
+            <img src="icon_dash.svg" alt="to dashboard" />{" "}
+          </Link>
+          <Link to="/dashboard" className={classes.buttons}>
+            <img src="icon_historic.svg" alt="to historic" />{" "}
+          </Link>
+          <Link to="/dashboard" className={classes.buttons}>
+            <img src="icon_profil.svg" alt="to profil" />{" "}
+          </Link>
+          <Link to="/dashboard" className={classes.buttons}>
+            <img src="icon_menu.svg" alt="to menu" />{" "}
+          </Link>
         </Toolbar>
       </AppBar>
     </React.Fragment>
   );
 }
 
-
-// class MenuBar extends React.Component {
-//   render() {
-//     return (
-//       <nav className="menu">
-//         <Link to="/dashboard">
-//           <img src="icon_dash.svg" alt="to dashboard" />{" "}
-//         </Link>
-//         <Link to="/menu">
-//           <img src="menu.svg" alt="to menu" />{" "}
-//         </Link>
-//       </nav>
-//     );
-//   }
-// }
-
-// export default MenuBar;
+export default MenuBar;
