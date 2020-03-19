ed52fe293ce3b36b4fa91ba34ae79781f8e575ff
Gregoire Lamy <g.aderlamy@gmail.com>
2020-03-19 20:01:09 +0100

    bravo les soignants et merge done ca marche

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
-const axios = require("axios");
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
