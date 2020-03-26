c99aa3f4eb109906a20b8611a08503a8eaf0981b
Gregoire Lamy <g.aderlamy@gmail.com>
2020-03-26 21:46:02 +0100

    legume working

--- client/src/components/dons/Card_available.js
+++ client/src/components/dons/Card_available.js
@@ -1,49 +1,79 @@
 import React from "react";
 
 class CarddonAvailable extends React.Component {
-    state={
-        isOpen:false,
-    }
-    toggleCard=()=>{
-        this.setState({isOpen:!this.state.isOpen})
-    }
-    render(){
-      return(
-        <div className="card_dons">
-            {!this.state.isOpen && (
-                <div className="cardClosed">
-                <p><img src="egg-basket.svg" alt="Logo panier" />Panier disponible</p>
-                <button onClick={this.toggleCard}><img src="icon_fleche_closed.svg" alt="Logo chevron closed"/></button>
-                </div>
-            )}
-            {this.state.isOpen && (
-                <div className ="cardOpen">
+  state = {
+    isOpen: false
+  };
+  toggleCard = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+  // imgProductType = productType => {
+  //   let img_product = "altIMG";
+  //   if (productType === "légume") {
+  //     img_product = "carrot.svg";
+  //   }
+  //   return img_product;
+  // };
+  render() {
+    const foodTypes = {
+      fruits: 'carrot.svg',
+      Légumes: 'carrot.svg',
+      viandes: 'carrot.svg',
+      divers: 'carrot.svg',
+    };
+    return (
+      <div className="card_dons">
+        {!this.state.isOpen && (
+          <div className="cardClosed">
+            <div>
+              <img src="Gift-Box.png" alt="Logo panier" />
+              Panier disponible
+            </div>
+            <div className="toggleButton">
+              <button onClick={this.toggleCard}>
+                <img src="icon_fleche_closed.svg" alt="Logo chevron closed" />
+              </button>
+            </div>
+          </div>
+        )}
+        {this.state.isOpen && (
+          <div className="cardOpen">
+            <div>
+              <p>
+                <img src="Gift-Box.png" alt="Logo panier" />
+                Panier disponible
+              </p>
+              <button onClick={this.toggleCard}>
+                <img src="icon_fleche_open.svg" alt="Logo chevron open " />
+                {this.state.isOpen}
+              </button>
+            </div>
+            <div>
+              {this.props.donationBox.map(unitDon => (
                 <div>
-                <p><img src="" alt="Logo panier" />Panier disponible</p>
-                <button onClick={this.toggleCard}><img src="icon_fleche_open.svg" alt="Logo chevron open "/>{this.state.isOpen}</button>
+                  <div>
+                    Type:
+                    {unitDon.productType}
+                    <img src={foodTypes[unitDon.productType]} alt='food type'/>
+                    <img src={this.props.img_product} alt="Logo productType" />
+                  </div>
+                  <div>{unitDon.productName}</div>
+
+                  <div>
+                    <p>Poids</p>
+                    <p>
+                      {unitDon.quantity.value} {" " + unitDon.quantity.qtyType}
+                    </p>
+                  </div>
+                  <div>
+                    <p>Date de peremption</p>
+                    <p>{unitDon.expirationDate}</p>
+                  </div>
                 </div>
-                <div>
-                {this.props.donationBox.map(unitDon => (
-              <div>
-                <p>
-                  {unitDon.productName}
-                  <img src="" alt="Logo productType" />
-                </p>
-                <p>
-                  Type:
-                  {unitDon.productType}
-                  <img src="" alt="Logo productType" />
-                </p>
-                <p>Poids</p>
-                <p>
-                  {unitDon.quantity.value} {" " + unitDon.quantity.qtyType}
-                </p>
-                <p>Date de peremption</p>
-                <p>{unitDon.expirationDate}</p>
-              </div>
-            ))}
-                </div>
-                <button>Modifier</button>{/*Renvoi au formulaire de don */}
+              ))}
+            </div>
+            <button>Modifier</button>
+            {/*Renvoi au formulaire de don */}
           </div>
         )}
       </div>
